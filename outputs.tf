61ea5c00d1f2f24cdb2250ef81d3eee300688ee8
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:32:15 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "stream_analytics_reference_input_mssqls_id" {
+  description = "Map of id values across all stream_analytics_reference_input_mssqls, keyed the same as var.stream_analytics_reference_input_mssqls"
+  value       = { for k, v in azurerm_stream_analytics_reference_input_mssql.stream_analytics_reference_input_mssqls : k => v.id }
+}
 output "stream_analytics_reference_input_mssqls_database" {
   description = "Map of database values across all stream_analytics_reference_input_mssqls, keyed the same as var.stream_analytics_reference_input_mssqls"
   value       = { for k, v in azurerm_stream_analytics_reference_input_mssql.stream_analytics_reference_input_mssqls : k => v.database }
