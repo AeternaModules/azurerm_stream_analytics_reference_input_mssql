c8415a9cafc5b421d50b118350f18823b372622f
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:00 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,55 +1,50 @@
-output "stream_analytics_reference_input_mssqls" {
-  description = "All stream_analytics_reference_input_mssql resources"
-  value       = azurerm_stream_analytics_reference_input_mssql.stream_analytics_reference_input_mssqls
-  sensitive   = true
-}
 output "stream_analytics_reference_input_mssqls_database" {
-  description = "List of database values across all stream_analytics_reference_input_mssqls"
-  value       = [for k, v in azurerm_stream_analytics_reference_input_mssql.stream_analytics_reference_input_mssqls : v.database]
+  description = "Map of database values across all stream_analytics_reference_input_mssqls, keyed the same as var.stream_analytics_reference_input_mssqls"
+  value       = { for k, v in azurerm_stream_analytics_reference_input_mssql.stream_analytics_reference_input_mssqls : k => v.database }
 }
 output "stream_analytics_reference_input_mssqls_delta_snapshot_query" {
-  description = "List of delta_snapshot_query values across all stream_analytics_reference_input_mssqls"
-  value       = [for k, v in azurerm_stream_analytics_reference_input_mssql.stream_analytics_reference_input_mssqls : v.delta_snapshot_query]
+  description = "Map of delta_snapshot_query values across all stream_analytics_reference_input_mssqls, keyed the same as var.stream_analytics_reference_input_mssqls"
+  value       = { for k, v in azurerm_stream_analytics_reference_input_mssql.stream_analytics_reference_input_mssqls : k => v.delta_snapshot_query }
 }
 output "stream_analytics_reference_input_mssqls_full_snapshot_query" {
-  description = "List of full_snapshot_query values across all stream_analytics_reference_input_mssqls"
-  value       = [for k, v in azurerm_stream_analytics_reference_input_mssql.stream_analytics_reference_input_mssqls : v.full_snapshot_query]
+  description = "Map of full_snapshot_query values across all stream_analytics_reference_input_mssqls, keyed the same as var.stream_analytics_reference_input_mssqls"
+  value       = { for k, v in azurerm_stream_analytics_reference_input_mssql.stream_analytics_reference_input_mssqls : k => v.full_snapshot_query }
 }
 output "stream_analytics_reference_input_mssqls_name" {
-  description = "List of name values across all stream_analytics_reference_input_mssqls"
-  value       = [for k, v in azurerm_stream_analytics_reference_input_mssql.stream_analytics_reference_input_mssqls : v.name]
+  description = "Map of name values across all stream_analytics_reference_input_mssqls, keyed the same as var.stream_analytics_reference_input_mssqls"
+  value       = { for k, v in azurerm_stream_analytics_reference_input_mssql.stream_analytics_reference_input_mssqls : k => v.name }
 }
 output "stream_analytics_reference_input_mssqls_password" {
-  description = "List of password values across all stream_analytics_reference_input_mssqls"
-  value       = [for k, v in azurerm_stream_analytics_reference_input_mssql.stream_analytics_reference_input_mssqls : v.password]
+  description = "Map of password values across all stream_analytics_reference_input_mssqls, keyed the same as var.stream_analytics_reference_input_mssqls"
+  value       = { for k, v in azurerm_stream_analytics_reference_input_mssql.stream_analytics_reference_input_mssqls : k => v.password }
   sensitive   = true
 }
 output "stream_analytics_reference_input_mssqls_refresh_interval_duration" {
-  description = "List of refresh_interval_duration values across all stream_analytics_reference_input_mssqls"
-  value       = [for k, v in azurerm_stream_analytics_reference_input_mssql.stream_analytics_reference_input_mssqls : v.refresh_interval_duration]
+  description = "Map of refresh_interval_duration values across all stream_analytics_reference_input_mssqls, keyed the same as var.stream_analytics_reference_input_mssqls"
+  value       = { for k, v in azurerm_stream_analytics_reference_input_mssql.stream_analytics_reference_input_mssqls : k => v.refresh_interval_duration }
 }
 output "stream_analytics_reference_input_mssqls_refresh_type" {
-  description = "List of refresh_type values across all stream_analytics_reference_input_mssqls"
-  value       = [for k, v in azurerm_stream_analytics_reference_input_mssql.stream_analytics_reference_input_mssqls : v.refresh_type]
+  description = "Map of refresh_type values across all stream_analytics_reference_input_mssqls, keyed the same as var.stream_analytics_reference_input_mssqls"
+  value       = { for k, v in azurerm_stream_analytics_reference_input_mssql.stream_analytics_reference_input_mssqls : k => v.refresh_type }
 }
 output "stream_analytics_reference_input_mssqls_resource_group_name" {
-  description = "List of resource_group_name values across all stream_analytics_reference_input_mssqls"
-  value       = [for k, v in azurerm_stream_analytics_reference_input_mssql.stream_analytics_reference_input_mssqls : v.resource_group_name]
+  description = "Map of resource_group_name values across all stream_analytics_reference_input_mssqls, keyed the same as var.stream_analytics_reference_input_mssqls"
+  value       = { for k, v in azurerm_stream_analytics_reference_input_mssql.stream_analytics_reference_input_mssqls : k => v.resource_group_name }
 }
 output "stream_analytics_reference_input_mssqls_server" {
-  description = "List of server values across all stream_analytics_reference_input_mssqls"
-  value       = [for k, v in azurerm_stream_analytics_reference_input_mssql.stream_analytics_reference_input_mssqls : v.server]
+  description = "Map of server values across all stream_analytics_reference_input_mssqls, keyed the same as var.stream_analytics_reference_input_mssqls"
+  value       = { for k, v in azurerm_stream_analytics_reference_input_mssql.stream_analytics_reference_input_mssqls : k => v.server }
 }
 output "stream_analytics_reference_input_mssqls_stream_analytics_job_name" {
-  description = "List of stream_analytics_job_name values across all stream_analytics_reference_input_mssqls"
-  value       = [for k, v in azurerm_stream_analytics_reference_input_mssql.stream_analytics_reference_input_mssqls : v.stream_analytics_job_name]
+  description = "Map of stream_analytics_job_name values across all stream_analytics_reference_input_mssqls, keyed the same as var.stream_analytics_reference_input_mssqls"
+  value       = { for k, v in azurerm_stream_analytics_reference_input_mssql.stream_analytics_reference_input_mssqls : k => v.stream_analytics_job_name }
 }
 output "stream_analytics_reference_input_mssqls_table" {
-  description = "List of table values across all stream_analytics_reference_input_mssqls"
-  value       = [for k, v in azurerm_stream_analytics_reference_input_mssql.stream_analytics_reference_input_mssqls : v.table]
+  description = "Map of table values across all stream_analytics_reference_input_mssqls, keyed the same as var.stream_analytics_reference_input_mssqls"
+  value       = { for k, v in azurerm_stream_analytics_reference_input_mssql.stream_analytics_reference_input_mssqls : k => v.table }
 }
 output "stream_analytics_reference_input_mssqls_username" {
-  description = "List of username values across all stream_analytics_reference_input_mssqls"
-  value       = [for k, v in azurerm_stream_analytics_reference_input_mssql.stream_analytics_reference_input_mssqls : v.username]
+  description = "Map of username values across all stream_analytics_reference_input_mssqls, keyed the same as var.stream_analytics_reference_input_mssqls"
+  value       = { for k, v in azurerm_stream_analytics_reference_input_mssql.stream_analytics_reference_input_mssqls : k => v.username }
 }
 
